d724af18645d68fd236460d135a3d1cf8db98c78
Miqueias Penha <miqueiaspenha@gmail.com>
2025-06-05 09:31:28 -0300

    feat: adiciona description em book

--- mini-leanpub/app/Domain/Book/Entity/Book.php
+++ mini-leanpub/app/Domain/Book/Entity/Book.php
@@ -9,12 +9,14 @@ class Book
 
     public function __construct(
         private ?string $id,
-        private ?string $title
+        private ?string $title,
+        private ?string $description
     ) {}
 
     public function validate()
     {
         if (!$this->id) throw new Exception('Invalid Entity: ID');
         if (!$this->title) throw new Exception('Invalid Entity: Title');
+        if (!$this->description) throw new Exception('Invalid Entity: Description');
     }
 }
