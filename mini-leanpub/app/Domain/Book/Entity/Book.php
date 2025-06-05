3b17c5f1f8167655d5a71543a3e92c424c46e372
Miqueias Penha <miqueiaspenha@gmail.com>
2025-06-05 09:28:30 -0300

    feat: adiciona title no book

--- mini-leanpub/app/Domain/Book/Entity/Book.php
+++ mini-leanpub/app/Domain/Book/Entity/Book.php
@@ -2,15 +2,19 @@
 
 namespace MiniLeanpub\Domain\Book\Entity;
 
+use Exception;
+
 class Book
 {
 
     public function __construct(
-        private ?string $id
+        private ?string $id,
+        private ?string $title
     ) {}
 
     public function validate()
     {
-        if (!$this->id) throw new \Exception('Invalid Entity: ID');
+        if (!$this->id) throw new Exception('Invalid Entity: ID');
+        if (!$this->title) throw new Exception('Invalid Entity: Title');
     }
 }
